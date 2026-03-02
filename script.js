fd6d451821eb141e65f11540051bf238f2514ccd
Nirjhar Bhattacharjee <bhattacharjeenirjhar26@gmail.com>
2026-03-02 21:55:49 +1100

    Create getComputerChoice function
    Create getComputerChoice function and generate random numbers between 1 and 3

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+let getComputerChoice = ()=>{
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(4);
+    let number = Math.floor(Math.random() *(maxFloored -minCeiled) + minCeiled);
+    return number;
+}
+console.log(getComputerChoice());
