a051069bf377e7e4327af06a12c98c3d01f3b9f3
Nirjhar Bhattacharjee <bhattacharjeenirjhar26@gmail.com>
2026-03-02 22:12:10 +1100

    Create numberToMove function to convert numbers to its move

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ let getComputerChoice = ()=>{
     const minCeiled = Math.ceil(1);
     const maxFloored = Math.floor(4);
     let number = Math.floor(Math.random() *(maxFloored -minCeiled) + minCeiled);
-    return number;
+   
+    let numberToMove= ()=>{
+        if(number === 1){
+            return "Rock";
+        }
+        else if (number ===2) {
+            return "Paper";
+        }
+        else{
+            return "Scissors";
+        }
+    }
+    return numberToMove();
 }
 console.log(getComputerChoice());
+
+// let getHumanChoice = ()=>{
+
+// }s
