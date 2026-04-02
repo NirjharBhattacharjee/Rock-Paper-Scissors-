151ec6b45eecf21890f1131d93381768ad84c494
Nirjhar Bhattacharjee <bhattacharjeenirjhar26@gmail.com>
2026-04-02 18:26:47 +1100

    create RPS buttons to display the selection
    create event listeners that listens to the click to display the input

--- script.js
+++ script.js
@@ -1,43 +1,25 @@
-let humanScore = 0;
-let computerScore = 0;
+const rock = document.querySelector(".rock-btn");
+const paper = document.querySelector(".paper-btn");
+const scissors = document.querySelector(".scissors-btn");
+const container = document.querySelector(".btn-container")
 
+const newDiv = document.createElement("div");
 
-let getComputerChoice = ()=>{
-    const minCeiled = Math.ceil(1);
-    const maxFloored = Math.floor(4);
-    let number = Math.floor(Math.random() *(maxFloored -minCeiled) + minCeiled);
-   
-    let numberToMove= ()=>{
-        if(number === 1){
-            return "Rock";
-        }
-        else if (number ===2) {
-            return "Paper";
-        }
-        else{
-            return "Scissors";
-        }
-    }
-    return numberToMove();
-}
-// console.log(getComputerChoice());
+rock.addEventListener("click", () => {
+    newDiv.classList.add("selection");
+    newDiv.textContent = "Rock";
+    container.appendChild(newDiv);
+});
 
-let getHumanChoice = ()=>{
-    let text = prompt("What's your choice? ");
-    let choice = text.toLowerCase();
-    // console.log(choice);
-    return choice;
-}
-// console.log(getHumanChoice());
+paper.addEventListener("click", () =>{
+    newDiv.classList.add("selection");
+    newDiv.textContent = "Paper";
+    container.appendChild(newDiv);
+});
 
-let playRound = (humanChoice,computerChoice)=>{
-    console.log("This is human choice: " + humanChoice);
-    // console.log(computerChoice);
-    return [humanChoice,computerChoice];
-}
+scissors.addEventListener("click", ()=>{
+    newDiv.classList.add("selection");
+    newDiv.textContent = "Scissors";
+    container.appendChild(newDiv);
+});
 
-const humanSelection = getHumanChoice();
-console.log(humanSelection);
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
